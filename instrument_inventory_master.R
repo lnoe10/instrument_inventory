4fa311406ab8018b88d13fc0d0f108d9d0013de6
Lorenz Noe <lorenznoe@gmail.com>
2021-07-27 18:42:56 -0400

    Import and clean time use survey listing

--- instrument_inventory_master.R
+++ instrument_inventory_master.R
@@ -179,7 +179,24 @@ ihsn <- fromJSON(content(GET("https://catalog.ihsn.org/index.php/api/catalog/sea
 
 #### Time use - UNSD ####
 # https://unstats.un.org/unsd/gender/timeuse and manual ODW check of NSO websites
-
+tus <- read_csv("C:/Users/lnoe/Documents/GitHub/instrument_inventory/Input/time_use_surveys_sgdf_inventory_2020.csv") %>%
+  janitor::clean_names() %>%
+  # Clean year column
+  separate(tus_years_available, into = c("year1", "year2"), sep = ", ") %>%
+  mutate(year1 = case_when(
+    year1 == "N/A" ~ NA_character_,
+    str_detect(year1, "/") ~ str_extract(year1, "(?<=/)[0-9]{4}"),
+    TRUE ~ year1
+  ),
+  across(year1:year2, as.numeric),
+  # Create other category variables
+  instrument_type = "Time Use Survey", source = "https://unstats.un.org/unsd/gender/timeuse and manual ODW check of NSO websites",
+  status = "Completed") %>%
+  # Convert to longer for few countries with more than 1 obs and then drop missing obs
+  pivot_longer(year1:year2, names_to = "indicator", values_to = "year") %>%
+  filter(!is.na(year)) %>%
+  select(country, iso3c = iso, year, instrument_name = time_use_survey, 
+         instrument_type, source, status)
 
 #### Census - UNSD ####
 # See scraping census dates file
