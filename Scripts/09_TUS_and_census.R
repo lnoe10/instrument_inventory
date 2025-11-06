e2fd241976f87c4fb8707824f053e6cb188fb0e0
lnoe10 <lorenznoe@gmail.com>
2025-11-06 17:48:11 -0500

    Formatting update of script
    %>% to |>

--- Scripts/09_TUS_and_census.R
+++ Scripts/09_TUS_and_census.R
@@ -6,10 +6,10 @@ library(tidyverse)
 
 # original TUS data based on Tawheeda's manual search
 # https://unstats.un.org/unsd/gender/timeuse and manual ODW check of NSO websites
-tus <- read_csv("Input/time_use_surveys_sgdf_inventory_2020.csv", show_col_types = F) %>%
-  janitor::clean_names() %>%
+tus <- read_csv("Input/time_use_surveys_sgdf_inventory_2020.csv", show_col_types = F) |>
+  janitor::clean_names() |>
   # Clean year column
-  separate(tus_years_available, into = c("year1", "year2"), sep = ", ") %>%
+  separate(tus_years_available, into = c("year1", "year2"), sep = ", ") |>
   mutate(year1 = case_when(
     year1 == "N/A" ~ NA_character_,
     str_detect(year1, "/") ~ str_extract(year1, "(?<=/)[0-9]{4}"),
@@ -18,10 +18,10 @@ tus <- read_csv("Input/time_use_surveys_sgdf_inventory_2020.csv", show_col_types
   across(year1:year2, as.numeric),
   # Create other category variables
   instrument_type = "Time Use Survey", source = "https://unstats.un.org/unsd/gender/timeuse and manual ODW check of NSO websites",
-  status = "Completed") %>%
+  status = "Completed") |>
   # Convert to longer for few countries with more than 1 obs and then drop missing obs
-  pivot_longer(year1:year2, names_to = "indicator", values_to = "year") %>%
-  filter(!is.na(year)) %>%
+  pivot_longer(year1:year2, names_to = "indicator", values_to = "year") |>
+  filter(!is.na(year)) |>
   select(country, iso3c = iso, year, instrument_name = time_use_survey, 
          instrument_type, source, status) |> 
   filter(year >= 2013)
@@ -113,9 +113,9 @@ xlsx::write.xlsx(tus_full, "Output/instrument_data_all_years/tus.xlsx")
 
 # See scraping census dates file
 # Census dates scrape.R
-census <- readRDS("Input/census_dates_df.rds") %>%
+census <- readRDS("Input/census_dates_df.rds") |>
   # Filter out where we only have housing census
-  filter(notes!="Housing census only."|is.na(notes)) %>%
+  filter(notes!="Housing census only."|is.na(notes)) |>
   mutate(planned = as.character(planned),
          planned = case_when(
            planned == 1 ~ "Planned",
@@ -125,7 +125,7 @@ census <- readRDS("Input/census_dates_df.rds") %>%
          instrument_type = "Census",
          source = "https://unstats.un.org/unsd/demographic-social/census/censusdates/",
          # remove parentheses for dates with just a year like (2023)-->2023
-         date = ifelse(str_detect(date, "^\\(\\d{4}\\)$"), str_remove_all(date, "\\(|\\)"), date)) %>%
+         date = ifelse(str_detect(date, "^\\(\\d{4}\\)$"), str_remove_all(date, "\\(|\\)"), date)) |>
   select(country, iso3c, year, status = planned, instrument_name, instrument_type, source, census_round, date)
 
 # export a list of censuses with status planned and are from 2023 or prior
