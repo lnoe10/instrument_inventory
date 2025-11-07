02c76ee8ec9e03263099ea222b0bba1ce589fafe
lnoe10 <lorenznoe@gmail.com>
2025-11-07 15:51:54 -0500

    Update pipe operator

--- Scripts/05_ag_surveys.R
+++ Scripts/05_ag_surveys.R
@@ -6,8 +6,8 @@ library(tidyverse)
 library(openxlsx)
 
 # First get data from API
-agri_survey_raw <- fromJSON(content(GET("https://microdata.fao.org/index.php/api/catalog/search?ps=10000"), "text"), flatten = TRUE)$result$rows %>%
-  as_tibble() %>%
+agri_survey_raw <- fromJSON(content(GET("https://microdata.fao.org/index.php/api/catalog/search?ps=10000"), "text"), flatten = TRUE)$result$rows |>
+  as_tibble() |>
   mutate(api_call = str_c("https://microdata.fao.org/index.php/api/catalog/", id, "?id_format=id"))
 
 # Check existing list of metadata in terms of what else we need. After importing new metadata,
@@ -42,13 +42,13 @@ new_ag_surveys <- agri_survey_raw |>
 # 
 # # Determine length of list generated from API call to filter out
 # # Where we hit error otherwise of subscript being out of bounds
-# study <- study %>%
+# study <- study |>
 #   mutate(length = case_when(
 #     !is_empty(fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)) ~ vec_depth(fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)),
 #     TRUE ~ NA_integer_))
 # # If list is deep enough, proceed to extract data
 # if(study$length > 3){
-#   study <- study %>%
+#   study <- study |>
 #     # Test first whether response is valid based on tunneling into list
 #     mutate(study_type = ifelse(is_empty(fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)$dataset$metadata$study_desc$series_statement$series_name), 
 #                                NA_character_,
@@ -73,7 +73,7 @@ new_ag_surveys <- agri_survey_raw |>
 #   #  
 #   #  # case when there is only a "name" and "abbreviation" available
 #   #  study <- study |> mutate(producers = fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)$dataset$metadata$study_desc$production_statement$producers |> 
-#   #                             as_tibble() %>%
+#   #                             as_tibble() |>
 #   #                             mutate(across(everything(), ~str_squish(.))) |> 
 #   #                             mutate(all_data = case_when(affiliation!="" ~ paste0(name, " (AFFILIATION: ", affiliation, ")"),
 #   #                                                         affiliation=="" ~ paste0(name))) |> 
@@ -82,7 +82,7 @@ new_ag_surveys <- agri_survey_raw |>
 #   #                             pull())
 #   #} else {
 #   #  study <- study |> mutate(producers = fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)$dataset$metadata$study_desc$production_statement$producers |> 
-#   #                             as_tibble() %>%
+#   #                             as_tibble() |>
 #   #                             mutate(across(everything(), ~str_squish(.))) |> 
 #   #                             mutate(all_data = case_when(affiliation!="" & role!="" ~ paste0(name, " (AFFILIATION: ", affiliation, ", ROLE: ", role, ")"),
 #   #                                                         affiliation=="" ~ paste0(name, ", (ROLE: ", role, ")"),
@@ -103,8 +103,8 @@ new_ag_surveys <- agri_survey_raw |>
 #   #  # case when there is only a "name" and "abbreviation" available
 #   #  study <- study |> mutate(funding_agencies = fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)$dataset$metadata$study_desc$production_statement$funding_agencies |> 
 #   #                             as_tibble() |> 
-#   #                             mutate(across(everything(), ~str_squish(.))) %>% 
-#   #                             mutate(abbreviation = ifelse("abbr" %in% names(.), abbr, abbreviation)) %>% 
+#   #                             mutate(across(everything(), ~str_squish(.))) |> 
+#   #                             mutate(abbreviation = ifelse("abbr" %in% names(.), abbr, abbreviation)) |> 
 #   #                             mutate(all_data = paste0(name, " (", abbreviation, ")")) |> 
 #   #                             select(all_data) |> 
 #   #                             summarise(funding_agencies = paste0(all_data, collapse = " // ")) |> 
@@ -112,8 +112,8 @@ new_ag_surveys <- agri_survey_raw |>
 #   #} else {
 #   #  study <- study |> mutate(funding_agencies = fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)$dataset$metadata$study_desc$production_statement$funding_agencies |> 
 #   #                             as_tibble() |> 
-#   #                             mutate(across(everything(), ~str_squish(.))) %>% 
-#   #                             mutate(abbreviation = ifelse("abbr" %in% names(.), abbr, abbreviation)) %>% 
+#   #                             mutate(across(everything(), ~str_squish(.))) |> 
+#   #                             mutate(abbreviation = ifelse("abbr" %in% names(.), abbr, abbreviation)) |> 
 #   #                             mutate(all_data = ifelse(role!="", paste0(name, " (", abbreviation, "), ROLE: ", role), paste0(name, " (", abbreviation, ")"))) |> 
 #   #                             select(all_data) |> 
 #   #                             summarise(funding_agencies = paste0(all_data, collapse = " // ")) |> 
@@ -128,7 +128,7 @@ new_ag_surveys <- agri_survey_raw |>
 #   } else if ("affiliation" %in% names(as_tibble(fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)$dataset$metadata$study_desc$authoring_entity))==FALSE) {
 #     # case when there is only a "name" available
 #     study <- study |> mutate(authoring_entity_detail = fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)$dataset$metadata$study_desc$authoring_entity |> 
-#                                as_tibble() %>%
+#                                as_tibble() |>
 #                                # need to use magrittr pipe for the period syntax to work
 #                                mutate(across(everything(), ~str_squish(.))) |> 
 #                                summarise(authoring_entity_detail = paste0(name, collapse = ", ")) |> 
@@ -137,9 +137,9 @@ new_ag_surveys <- agri_survey_raw |>
 #     study <- study |> mutate(authoring_entity_detail = ifelse(is_empty(fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)$dataset$metadata$study_desc$authoring_entity),
 #                                                               NA_character_,
 #                                                               fromJSON(content(GET(new_ag_surveys$api_call[i]), "text"), flatten = TRUE)$dataset$metadata$study_desc$authoring_entity |> 
-#                                                                 as_tibble() %>%
+#                                                                 as_tibble() |>
 #                                                                 # need to use magrittr pipe for the period syntax to work
-#                                                                 mutate(across(everything(), ~str_squish(.)))  %>%
+#                                                                 mutate(across(everything(), ~str_squish(.)))  |>
 #                                                                 mutate(authoring_entity = ifelse("affiliation" %in% names(.) & affiliation!="", paste0(name, " (", affiliation, ")"), name)) |> 
 #                                                                 # remove empty rows
 #                                                                 filter(authoring_entity!="") |> 
@@ -150,7 +150,7 @@ new_ag_surveys <- agri_survey_raw |>
 # }
 # 
 # # Append to dataset
-# new_fao_metadata <- new_fao_metadata %>%
+# new_fao_metadata <- new_fao_metadata |>
 #   bind_rows(study)
 # # Insert brief pause in code to not look like a robot to the API
 # Sys.sleep(sample(seq(0,0.3,by=0.001),1))
@@ -168,26 +168,26 @@ new_ag_surveys <- agri_survey_raw |>
 #saveRDS(all_fao_metadata, file = "Input/fao_ag_survey_metadata.rds")
 
 # Set up final list of Agricultural Surveys
-agri_survey <- agri_survey_raw %>%
+agri_survey <- agri_survey_raw |>
   left_join(all_fao_metadata, by="id") |> 
   mutate(year_end = as.numeric(year_end),
          iso3c = countrycode::countrycode(nation, "country.name", "iso3c"),
          instrument_type = "Agricultural Survey/Census",
          status = "completed",
-         source = "https://microdata.fao.org/index.php/catalog") %>%
+         source = "https://microdata.fao.org/index.php/catalog") |>
   # Filter for study type and repositories
   # Add AGRISurvey info from Tawheeda and LSMS+ Ag modules
   bind_rows(tribble(
     ~title, ~nation, ~iso3c, ~year_end, ~instrument_type, ~status, ~source, ~study_type, ~repositoryid,
     "Encuesta Nacional Agropecuaria", "Costa Rica", "CRI", 2019, "Agricultural Survey/Census", "Completed", "http://www.fao.org/documents/card/en/c/cb3976en", "Agriculture Integrated Survey[AGRISurvey]", "agricultural-surveys",
     "AGRISurvey Nation-wide", "Nepal", "NPL", 2020, "Agricultural Survey/Census", "Completed", "http://www.fao.org/in-action/agrisurvey/country-work/nepal/en/", "Agriculture Integrated Survey[AGRISurvey]", "agricultural-surveys"
-  )) %>%
+  )) |>
   filter(repositoryid %in% c("agricultural-surveys", "50x2030"),
          !study_type %in% c("Administrative Records", "Agricultural Census [ag/census]", "Enterprise Census [en/census]", 
                             "Population and Housing Census [hh/popcen]", "Living Standards Measurement Study [hh/lsms]", 
                             "Income/Expenditure/Household Survey [hh/ies]", "Socio-Economic/Monitoring Survey [hh/sems]",
                             "Other Household Survey [hh/oth]", "Integrated Survey (non-LSMS) [hh/is]"), 
-         !str_detect(title, "mpact|roduction|ensus|oldings|Good Growth Plan|Stock|isheries|ivestock|oultry|Corn|Fish|fish|Avian|Aquaculture|Losses|Conservation|Pesticide|Commodities")) %>%
+         !str_detect(title, "mpact|roduction|ensus|oldings|Good Growth Plan|Stock|isheries|ivestock|oultry|Corn|Fish|fish|Avian|Aquaculture|Losses|Conservation|Pesticide|Commodities")) |>
   distinct(iso3c, year_end, .keep_all = TRUE) |>
   select(id, country = nation, iso3c, year = year_end, instrument_name = title, repositoryid, instrument_type, status, source, authoring_entity, 
          study_type, unit_of_analysis, data_kind, universe, producers, authoring_entity_detail, funding_agencies)
